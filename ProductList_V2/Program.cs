71e503d5403e60311f41f058310187a4686faec1
Karamellen123 <ellen.s.davidsson@gmail.com>
2024-02-15 11:36:11 +0100

    Added code from previous none working

--- ProductList_V2/Program.cs
+++ ProductList_V2/Program.cs
@@ -1,2 +1,40 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using System.Text.RegularExpressions;
+
 Console.WriteLine("Hello, World!");
+//using System.Text.RegularExpressions;
+
+List<string> ProductLists = new List<string>();
+
+MainMenu();
+
+void printList()
+{
+    Console.WriteLine("Printing list");
+    for (int i = 0; i < ProductLists.Count; i++)
+        Console.WriteLine(i + ") " + ProductLists[i]);
+}
+
+void AddProduct()
+{
+    Console.WriteLine("What product would you like to add? ");
+    string temp = Console.ReadLine();
+
+    //if (temp.Contains("exit"))
+    Regex exitRegex = new Regex(@"\b(exit|quit|end)\b", RegexOptions.IgnoreCase);
+
+    // Check if the input matches the pattern
+    if (exitRegex.IsMatch(temp))
+        printList();
+    else
+    {
+        ProductLists.Add(temp);
+        Console.WriteLine("You have added: " + temp + " to your product list");
+        MainMenu();
+    }
+}
+
+void MainMenu()
+{
+    AddProduct();
+}
